0df8fc1dda4a5577bd176c2547d18992334b5070
Your Name <bhoomahrishi@gmail.com>
2025-03-09 04:17:40 -0400

    im sleepee

--- back/find_stocks.py
+++ back/find_stocks.py
@@ -4,22 +4,23 @@ import openai
 from openai import OpenAI
 
 # Load environment variables from the .env file
-load_dotenv('secrets.env')
+load_dotenv()
 
 # Function to fetch stock list based on the prompt
 def stock_list(prompt):
     # Ensure the API key is loaded from environment variables
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    api_key = os.environ.get('OPENAI_API_KEY')
+    openai.api_key = api_key
 
     client = OpenAI()
-    
+
     # Create a completion request to GPT-4
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-4o",
         messages=[
             {
                 "role": "system",
-                "content": "Generate a space-separated string of at most 10 stock symbols based on the following prompt: " + prompt
+                "content": "Generate a space-separated string of 9 stock symbols based on the following prompt: " + prompt
             },
             {
                 "role": "user",
@@ -34,16 +35,17 @@ def stock_list(prompt):
     )
     
     # Return the text of the response
-    return response.choices[0].message.content.split()[:10]
+    return response.choices[0].message.content.split()[:9]
 
 def risk_measure(prompt):
     # Ensure the API key is loaded from environment variables
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    api_key = os.environ.get('OPENAI_API_KEY')
+    openai.api_key = api_key
 
     client = OpenAI()
 
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-4o",
         messages=[
             {
             "role": "system",
---
+++ test.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        ppt = request.form["nm"]
+        if ppt:
+            return redirect(url_for("stocks", prompt=ppt))
+        else:
+            return render_template("index.html")
+    else:
+        return render_template("index.html")
+
+@app.route("/stocks.html")
+def stocks():
+    prompt = request.args.get("prompt")
+    a=['AAPL', 'MSFT', 'TSLA', 'AMZN', 'GOOGL', 'META', 'NVDA', 'NFLX', 'ADBE']
+    b=[145.67, 310.23, 670.50, 3350.25, 2800.65, 350.45, 220.75, 620.30, 680.90]
+    return render_template("stocks.html", x=a, y=b)
+
+@app.route("/results.html")
+def results():
+    return render_template("results.html")
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
